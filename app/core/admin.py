f5b4d5b69cc8cd51c1c2a4bdb03b979e971e793a
ivan570 <ivankshu@gmail.com>
2023-10-17 11:42:02 +0530

    somechanges

--- app/core/admin.py
+++ app/core/admin.py
@@ -13,10 +13,18 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Permissions'),
-         {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-        (_('Important dates'),
-         {'fields': ('last_login', )}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login', )}),
     )
     readonly_fields = ['last_login']
     add_fieldsets = (
